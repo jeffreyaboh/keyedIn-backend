f7d34146d7c9ffaa78e948e11339845c03414804
jeffreyaboh <jefferyaboh@gmail.com>
2021-02-28 20:49:27 -0800

    Seeders Changes

--- app/server.js
+++ app/server.js
@@ -2,11 +2,14 @@
 var app = require('./app');
 var port = process.env.PORT || 3000;
 var http = require('http').createServer(app);
+var categorySeeder = require('../seeds/CategoryTableSeeder');
 
 // Default launch URL
 app.use('/', function(req, res) {
     res.status(200).send('KeyedIn API Server is Running');
 });
+
+categorySeeder.run();
 
 // Starting API Server
 var server = http.listen(port, function() {
--- seeds/CategoryTableSeeder.js
+++ seeds/CategoryTableSeeder.js
@@ -1,14 +1,32 @@
+const { ModelBuildInstance } = require('twilio/lib/rest/autopilot/v1/assistant/modelBuild');
 var categorySchema = require('../schema/CategoriesSchema');
-
-function run() {
-    const table = {
+var categoryData = [
+    {
         name: 'Computing and ICT'
     }
-    categorySchema.findOne({ name: table.name }, function(reject, resolve) {
-        if (!resolve) {
-            var saveTable = new categorySchema(table);
-            saveTable.save();
-        }
-    })
+]
 
+module.exports = {
+    run: function run() {
+        console.log('function is running')
+        categorySchema.find({}, function(reject, resolve) {
+            if (resolve) {
+                resolve.forEach((data) => {
+                    categoryData.forEach((category) => {
+                        if (data.name != category.name) {
+                            var newData = new categorySchema (category);
+                            newData.save();
+                            console.log('saved to database!')
+                        }
+                    })
+                })
+            }
+            if (!resolve) {
+                var newData = new categorySchema (categoryData);
+                newData.save();
+                console.log('saved new to database!')
+            }
+        })
+    
+    }
 }
